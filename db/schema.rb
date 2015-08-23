7272c716a5554f3ca566d7b32aeb95b33d5b6551
Homan <boa.homan@gmail.com>
2015-08-23 23:48:05 +0800

    Some scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -16,66 +16,11 @@ ActiveRecord::Schema.define(version: 20150823153648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-
-  create_table "grades", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "key",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "matches", force: :cascade do |t|
-    t.string   "grade",          limit: 255
-    t.date     "date"
-    t.string   "opponent",       limit: 255
-    t.string   "winner",         limit: 255
-    t.integer  "score"
-    t.integer  "opponent_score"
-    t.string   "result",         limit: 255
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "location"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.float    "points_a"
-    t.float    "points_b"
-    t.float    "points_c"
-    t.float    "points_d"
-    t.float    "points_e"
-    t.float    "points_f"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.float    "points_v1"
-    t.float    "points_v2"
-    t.float    "points_ld1"
-    t.float    "points_ld2"
   end
 
   create_table "users", force: :cascade do |t|
