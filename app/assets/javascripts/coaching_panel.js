c2ea3dfaad0d4846ebd9921ae3ecfa1da252a9ed
Boa Ho man <boa.homan@gmail.com>
2015-11-08 23:51:37 +0800

    Coaching panel vertical menu

---
+++ app/assets/javascripts/coaching_panel.js
@@ -0,0 +1,27 @@
+var main = function() {
+  /* Push the body and the nav over by 250px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "250px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-250px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
+$(document).on('page:load', main);
