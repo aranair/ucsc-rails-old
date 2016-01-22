6a68bfb1bb0a30d3161f7ee5c3db710deb43075b
Boa Ho man <boa.homan@gmail.com>
2016-01-22 22:49:35 +0800

    Config nginx and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'ucsc'
 set :repo_url, 'git@github.com:aranair/ucsc.git'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
