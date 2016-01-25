a6f7a84fe6d5fc36da852d6df24145bdeb6d95d6
Boa Ho man <boa.homan@gmail.com>
2016-01-25 23:45:19 +0800

    Cleanups

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,30 +1,19 @@
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(44.5403, -78.5463),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var mapCanvas = document.getElementById('map');
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-// google.maps.event.addDomListener(window, 'load', initialize);
-
 $(document).ready(function() {
   $('.ui.dropdown').dropdown();
   $('body').scrollspy({ target: '#home-top-navbar' })
 
-  var parallax = document.querySelectorAll(".parallax"),
-      speed = 0.5;
+  // var parallax = document.querySelectorAll(".parallax"),
+  //     speed = 0.5;
 
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
+  // window.onscroll = function(){
+  //   [].slice.call(parallax).forEach(function(el,i){
 
-      var windowYOffset = window.pageYOffset,
-          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+  //     var windowYOffset = window.pageYOffset,
+  //         elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
 
-      el.style.backgroundPosition = elBackgrounPos;
+  //     el.style.backgroundPosition = elBackgrounPos;
 
-    });
-  };
+  //   });
+  // };
 
 });
