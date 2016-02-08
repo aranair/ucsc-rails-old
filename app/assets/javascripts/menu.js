b42cbcbd2dd8a62d1c6b3eba0da507425f66ed27
Boa Ho man <boa.homan@gmail.com>
2016-02-08 14:15:32 +0800

    Menu js

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -11,15 +11,5 @@ var closeMenu = function() {
   $('.full-menu').toggleClass('full-menu--open');
 }
 
-var openMenu = function() {
-  $('.menu').animate({
-    left: "0px"
-  }, 200);
-
-  $('body').animate({
-    left: "250px"
-  }, 200);
-}
-
 $(document).ready(bindMenu);
 $(document).on('page:load', bindMenu);
