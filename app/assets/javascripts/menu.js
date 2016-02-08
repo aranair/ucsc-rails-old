ec421a892bcc0f6d35e2a4dc2b467268c76126cd
Boa Ho man <boa.homan@gmail.com>
2016-02-08 14:04:49 +0800

    Better menu

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,25 +1,14 @@
 var bindMenu = function() {
-  $('.icon-menu').click(moveMenu)
+  $('.icon-menu').click(moveMenu);
   $('.icon-close').click(moveMenu);
 };
 
 var moveMenu = function() {
-  var $menu = $('.menu')
-  if ($menu.position().left == 0) {
-    closeMenu();
-  } else {
-    openMenu();
-  }
+  $('.full-menu').toggleClass('full-menu--open');
 }
 
 var closeMenu = function() {
-  $('.menu').animate({
-    left: "-250px"
-  }, 200);
-
-  $('body').animate({
-    left: "0px"
-  }, 200);
+  $('.full-menu').toggleClass('full-menu--open');
 }
 
 var openMenu = function() {
